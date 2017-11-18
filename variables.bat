865d3ab118421c42ece49ec3a8ffe64838fd9a5c
dkublik <dkublik@gmail.com>
2017-11-18 15:27:41 +0100

    variables

---
+++ variables.bat
@@ -0,0 +1,7 @@
+setx DOCKER_TOOLBOX_INSTALL_PATH "E:\programy\Docker Toolbox"
+setx PYTHON_HOME "C:\Users\dk\AppData\Local\Programs\Python\Python36-32"
+setx CURL_HOME "E:\programy\curl-7.56.1-win64-mingw"
+setx NPM_HOME "C:\Users\dk\AppData\Roaming\npm"
+setx JAVA_HOME "E:\programy\Java\jdk1.8.0_92"
+
+setx Path "%JAVA_HOME%\bin;%PYTHON_HOME%;%PYTHON_HOME%\Scripts;%NPM_HOME%;%CURL_HOME%\bin;%DOCKER_TOOLBOX_INSTALL_PATH%"
